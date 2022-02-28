7dee5a1f835bdb1c9328e16f1a21084ca163d995
Quan <quantrung286@gmail.com>
2022-02-28 23:34:43 +0700

    up

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import memories from './images/memories.png'
-import useStyles from './styles';
+import useStyles from './styles'
 
 const App = () => {
-  const classes = useStyles()
+	const classes = useStyles()
 
 	return (
 		<Container maxWidth='lg'>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import Post from './Post/Post'
+import useStyles from './styles'
 
 const Posts = () => {
+  const classes = useStyles()
+
   return (
     <React.Fragment>
     <h1>POSTS</h1>
