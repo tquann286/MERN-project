5de2e25fb117df3b1961c735e618a2158871a027
Quan <quantrung286@gmail.com>
2022-03-01 17:14:49 +0700

    up

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -25,3 +25,15 @@ export const createPost = async (req, res) => {
 		res.status(409).json({ message: error.message })
 	}
 }
+
+export const updatePost = async (req, res) => {
+	const { id: _id } = req.params
+	const post = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(id))
+		return res.status(404).send(`No post with id: ${id}`)
+
+	const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true })
+
+	res.json(updatedPost);
+}
