a77e108d6e0f18d3d0c54bca77e159393a5f6067
Quan <quantrung286@gmail.com>
2022-03-03 20:36:24 +0700

    up

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -10,9 +10,15 @@ import {
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 
 import useStyles from './styles'
+import Input from './Input'
 
 const Auth = () => {
+	const [form, setForm] = useState(initialState)
+	const [isSignup, setIsSignup] = useState(false)
 	const classes = useStyles()
+
+	const handleSubmit = (e) => {}
+	const handleChange = (e) => {}
 
 	return (
 		<Container component='main' maxWidth='xs'>
@@ -20,6 +26,30 @@ const Auth = () => {
 				<Avatar className={classes.avatar}>
 					<LockOutlinedIcon />
 				</Avatar>
+				<Typography component='h1' variant='h5'>
+					{isSignup ? 'Sign up' : 'Sign in'}
+				</Typography>
+				<form className={classes.form} onSubmit={handleSubmit}>
+					<Grid container spacing={2}>
+						{isSignup && (
+							<React.Fragment>
+								<Input
+									name='firstName'
+									label='First Name'
+									handleChange={handleChange}
+									autoFocus
+									half
+								/>
+								<Input
+									name='lastName'
+									label='Last Name'
+									handleChange={handleChange}
+									half
+								/>
+							</React.Fragment>
+						)}
+					</Grid>
+				</form>
 			</Paper>
 		</Container>
 	)
