32521bf95f023380f958206fb3bca8785e5bb16d
Quan <quantrung286@gmail.com>
2022-03-04 12:54:07 +0700

    up

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -12,3 +12,15 @@ export const signin = (formData, router) => async (dispatch) => {
 		console.log(error)
 	}
 }
+
+export const signup = (formData, router) => async (dispatch) => {
+	try {
+		const { data } = await api.signUp(formData)
+
+		dispatch({ type: AUTH, data })
+
+		router.push('/')
+	} catch (error) {
+		console.log(error)
+	}
+}
--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import postRoutes from './routes/posts.js'
 
+import postRoutes from './routes/posts.js'
+import userRouter from './routes/user.js'
 
 const app = express()
 dotenv.config()
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
+app.use('/user', userRouter)
 
 app.get('/', (req, res) => {
 	res.send('Hello to Memories API')
@@ -22,7 +24,10 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000
 
 mongoose
-	.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(process.env.CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then(() =>
 		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 	)
