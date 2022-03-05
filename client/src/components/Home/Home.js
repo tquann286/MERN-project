e92903eeb9c6a39f4e65dafcf1e48f798f917e82
Quan <quantrung286@gmail.com>
2022-03-05 19:37:31 +0700

    up

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -17,6 +17,8 @@ import Posts from '../Posts/Posts'
 import Form from '../Form/Form'
 import Pagination from '../Pagination'
 
+import useStyles from './styles'
+
 function useQuery() {
 	return new URLSearchParams(useLocation().search)
 }
@@ -24,7 +26,32 @@ function useQuery() {
 const Home = () => {
 	const [currentId, setCurrentId] = useState(0)
 	const dispatch = useDispatch()
-  const query = useQuery();
+	const query = useQuery()
+	const page = query.get('page') || 1
+	const searchQuery = query.get('searchQuery')
+	const history = useHistory()
+	const classes = useStyles()
+
+	const [search, setSearch] = useState('')
+	const [tags, setTags] = useState([])
+
+	const handleKeyPress = (e) => {
+		if (e.keyCode === 13) {
+			searchPost()
+		}
+	}
+
+	const searchPost = () => {
+		if (search.trim() || tags) {
+		} else {
+			history.push('/')
+		}
+	}
+
+	const handleAdd = (tag) => setTags([...tags, tag])
+
+	const handleDelete = (tagToDelete) =>
+		setTags(tags.filter((tag) => tag !== tagToDelete))
 
 	useEffect(() => {
 		dispatch(getPosts())
@@ -32,17 +59,49 @@ const Home = () => {
 
 	return (
 		<Grow in>
-			<Container>
+			<Container maxWidth='xl'>
 				<Grid
 					container
 					justifyContent='space-between'
 					alignItems='stretch'
 					spacing={3}
+					className={classes.gridContainer}
 				>
-					<Grid item xs={12} sm={7}>
+					<Grid item xs={12} sm={6} md={9}>
 						<Posts setCurrentId={setCurrentId} />
 					</Grid>
-					<Grid item xs={12} sm={4}>
+					<Grid item xs={12} sm={6} md={3}>
+						<AppBar
+							className={classes.appBarSearch}
+							position='static'
+							color='inherit'
+						>
+							<TextField
+								onKeyDown={handleKeyPress}
+								name='search'
+								variant='outlined'
+								label='Search Memories'
+								fullWidth
+								value={search}
+								onChange={(e) => setSearch(e.target.value)}
+							/>
+							<ChipInput
+								style={{ margin: '10px 0' }}
+								value={tags}
+								onAdd={handleAdd}
+								onDelete={handleDelete}
+								label='Search Tags'
+								variant='outlined'
+							/>
+							<Button
+								onClick={searchPost}
+								className={classes.searchButton}
+								variant='contained'
+								color='primary'
+							>
+								Search
+							</Button>
+						</AppBar>
 						<Form currentId={currentId} setCurrentId={setCurrentId} />
 						<Paper elevation={6}>
 							<Pagination />
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -74,7 +74,7 @@ const Post = ({ post, setCurrentId }) => {
 						style={{ color: 'white' }}
 						size='small'
 					>
-						<MoreHorizIcon fontSize='default' />
+						<MoreHorizIcon fontSize='medium' />
 					</Button>
 				</div>
 			)}
